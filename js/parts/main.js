97e387bb3902ec61545839afec9761a9517487b2
Jacky Nguyen <jackytnguyen@hotmail.com>
2023-09-21 13:35:16 +0200

    Add folders to the project

---
+++ js/parts/main.js
@@ -0,0 +1,29 @@
+jQuery(function($) {
+
+  $(window).load(function() {
+
+    var $gal = $("#propertyThumbnails"),
+      galW = $gal.outerWidth(true),
+      galSW = $gal[0].scrollWidth,
+      wDiff = (galSW / galW) - 1, // widths difference ratio
+      mPadd = 60, // Mousemove Padding
+      damp = 20, // Mousemove response softness
+      mX = 0, // Real mouse position
+      mX2 = 0, // Modified mouse position
+      posX = 0,
+      mmAA = galW - (mPadd * 2), // The mousemove available area
+      mmAAr = (galW / mmAA); // get available mousemove fidderence ratio
+
+    $gal.mousemove(function(e) {
+      mX = e.pageX - $(this).offset().left;
+      mX2 = Math.min(Math.max(0, mX - mPadd), mmAA) * mmAAr;
+    });
+
+    setInterval(function() {
+      posX += (mX2 - posX) / damp; // zeno's paradox equation "catching delay"	
+      $gal.scrollLeft(posX * wDiff);
+    }, 10);
+
+  });
+
+});
